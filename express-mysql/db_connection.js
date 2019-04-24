ec50b853c3c993e59e45535d94f42eb522fc70ee
dumbdumbdumb <edbm95@naver.com>
2019-04-23 21:08:52 -0700

    mysql connection,  and query test

---
+++ express-mysql/db_connection.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: 'dkssud12',
+    database: 'okey'
+})
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("good")
+});
+
+module.exports = connection;
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 var landing  = require('../controllers/landing');
+var db_connection = require('../express-mysql/db_connection');
 
+
+/*DB query test*/
+db_connection.query("SELECT * FROM device_info", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+});
 
 /* GET home page. */
 router.get('/',landing.get_landing);
+router.post('/',landing.submit_lead);
+
+
 
 
 module.exports = router;
