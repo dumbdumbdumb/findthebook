6b2237af44f24abc1e402e2c3ec1a1c29e04924c
dumbdumbdumb <edbm95@naver.com>
2019-05-16 16:13:47 -0700

    Invalid student ID check (added)

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,50 +1,61 @@
-var models = require('../models')
+var models = require('../models');
 
 
 exports.get_landing = function(req, res, next) {
     res.render('landing', { title: 'Express' });
-}
+};
 
 exports.submit_studentID = function(req, res, next) {
-        return models.books.findAll ({
-            include:[{
-                model: models.classes,
-                required: true,
+    models.students.findOne({where: {student_id: req.body.lead_id}}).then(function (validInvalid) {
+        if(validInvalid == null){
+            //if student_ID invalid ==> alarm
+
+            res.render('landing', { title: 'Express',invalid:1});
+
+        }else{
+            //student_ID valid ==> search & print out
+
+            models.books.findAll ({
                 include:[{
-                    model: models.schedules,
+                    model: models.classes,
                     required: true,
-                        where: {
-                            student_id: req.body.lead_id
-                        }
+                    include:[{
+                        model: models.schedules,
+                        required: true,
+                            where: {
+                                student_id: req.body.lead_id
+                            }
+                    }]
                 }]
-            }]
 
+            }
+            ).then(function(book){
+                var resObj = book.map(function (book){
+                    return Object.assign(
+                        {},
+                        {
+                            book_title: book.book_title,
+                            book_publisher: book.book_publisher,
+                            book_location: book.location,
+                            class_info: book.classes.map(function (classes) {
+                                return Object.assign(
+                                    {},
+                                    {
+                                        class_title: classes.class_title
+                                    }
+                                )
+                            })
+                        }
+                    );
+                });
+                console.log(resObj);
+                //res.json(resObj);
+                res.render('list', { title: 'Express', leads: resObj });
+
+            })
         }
-        ).then(function(book){
-            //res.json(book);
-            var resObj = book.map(function (book){
-                return Object.assign(
-                    {},
-                    {
-                        book_title: book.book_title,
-                        book_publisher: book.book_publisher,
-                        book_location: book.location,
-                        class_info: book.classes.map(function (classes) {
-                            return Object.assign(
-                                {},
-                                {
-                                    class_title: classes.class_title
-                                }
-                            )
-                        })
-                    }
-                );
-            });
-            console.log(resObj);
-            //res.json(resObj);
-            res.render('list', { title: 'Express', leads: resObj });
 
-        })
-}
+    })
+};
 
 
